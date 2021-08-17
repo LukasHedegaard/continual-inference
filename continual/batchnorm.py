e9d9685d1ea1b0c561ac77c65b8756839c9beebc
LukasHedegaard <lh@eng.au.dk>
2021-08-17 12:43:29 +0200

    Remove obsolete BatchNorm2d

--- continual/batchnorm.py
+++ continual/batchnorm.py
@@ -1,71 +1,11 @@
-import torch
-from torch import Tensor, nn
-from torch.nn.modules.batchnorm import _BatchNorm
+def normalise_momentum(num_steps: int, base_mom=0.1):
+    """Normalise momentum in BatchNorm for frame-by-frame updates
 
-from .interface import CoModule
-from .utils import temporary_parameter
+    Args:
+        num_steps (int): Number of steps considered in a time-series
+        base_mom (float, optional): Momentum used if the whole time-series was processed at once. Defaults to 0.1.
 
-
-def normalise_momentum(num_frames: int, base_mom=0.1):
-    return 2 / (num_frames * (2 / base_mom - 1) + 1)
-
-
-class BatchNorm2d(_BatchNorm, CoModule):
-    def __init__(
-        self,
-        num_features,
-        eps=1e-5,
-        momentum=0.1,
-        affine=True,
-        track_running_stats=True,
-        window_size=1,
-    ):
-        super(BatchNorm2d, self).__init__(
-            num_features, eps, momentum, affine, track_running_stats
-        )
-        # Normalise momentum w.r.t. the expected clip size
-        self.momentum = normalise_momentum(window_size, momentum)
-        self.unnormalised_momentum = momentum
-
-    def _check_input_dim(self, input):
-        # This function is called from _BatchNorm.forward to validate the input shape
-        # In this module, we need to cover the same cases as both BatchNorm1d and BatchNorm2d
-        if input.dim() not in {2, 3, 4}:
-            raise ValueError(
-                "expected 2D, 3D, or 4D input (got {}D input)".format(input.dim())
-            )
-
-    def forward_step(self, input: Tensor) -> Tensor:
-        output = _BatchNorm.forward(self, input)
-        return output
-
-    def forward_steps(self, input: Tensor) -> Tensor:
-        return self.forward(input)
-
-    def forward(self, input: Tensor) -> Tensor:
-        with temporary_parameter(self, "momentum", self.unnormalised_momentum):
-            output = _BatchNorm.forward(self, input)
-        return output
-
-    @staticmethod
-    def build_from(module: nn.BatchNorm2d):
-        comodule = BatchNorm2d(
-            num_features=module.num_features,
-            eps=module.eps,
-            momentum=module.momentum,
-            affine=module.affine,
-            track_running_stats=module.track_running_stats,
-            window_size=1,
-        )
-
-        with torch.no_grad():
-            comodule.load_state_dict(module.state_dict())
-
-        return comodule
-
-    @property
-    def delay(self) -> int:
-        return 0
-
-    def clean_state(self):
-        ...
+    Returns:
+        float: Momentum normalised to `num_steps`-fold update-steps
+    """
+    return 2 / (num_steps * (2 / base_mom - 1) + 1)
