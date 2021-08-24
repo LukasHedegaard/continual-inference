e7dd2682d8e5d9fa216ad0ff985e88d473d4584d
LukasHedegaard <lh@eng.au.dk>
2021-08-24 09:59:46 +0200

    Add test for temporary_parameter

---
+++ tests/continual/test_utils.py
@@ -0,0 +1,15 @@
+from continual.utils import temporary_parameter
+
+
+def test_temporary_parameter():
+    class MyClass:
+        def __init__(self) -> None:
+            self.x = 0
+
+    c = MyClass()
+    assert c.x == 0
+
+    with temporary_parameter(c, "x", 42):
+        assert c.x == 42
+
+    assert c.x == 0
