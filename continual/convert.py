6ea57f477fd2e9e20fe68b6532d6badebc1de909
LukasHedegaard <lh@eng.au.dk>
2022-05-04 15:59:25 +0200

    Remove redundant ptflops registration

--- continual/convert.py
+++ continual/convert.py
@@ -208,30 +208,3 @@ register(nn.Sequential, Sequential)
 
 # Closure
 register(FunctionType, Lambda)
-
-
-# Register modules in `ptflops`
-try:
-    from ptflops import flops_counter as fc
-
-    # Conv
-    fc.MODULES_MAPPING[Conv1d] = fc.conv_flops_counter_hook
-    fc.MODULES_MAPPING[Conv2d] = fc.conv_flops_counter_hook
-    fc.MODULES_MAPPING[Conv3d] = fc.conv_flops_counter_hook
-
-    # Pooling
-    fc.MODULES_MAPPING[AvgPool1d] = fc.pool_flops_counter_hook
-    fc.MODULES_MAPPING[MaxPool1d] = fc.pool_flops_counter_hook
-    fc.MODULES_MAPPING[AvgPool2d] = fc.pool_flops_counter_hook
-    fc.MODULES_MAPPING[MaxPool2d] = fc.pool_flops_counter_hook
-    fc.MODULES_MAPPING[AdaptiveAvgPool2d] = fc.pool_flops_counter_hook
-    fc.MODULES_MAPPING[AdaptiveMaxPool2d] = fc.pool_flops_counter_hook
-    fc.MODULES_MAPPING[AvgPool3d] = fc.pool_flops_counter_hook
-    fc.MODULES_MAPPING[MaxPool3d] = fc.pool_flops_counter_hook
-    fc.MODULES_MAPPING[AdaptiveAvgPool3d] = fc.pool_flops_counter_hook
-    fc.MODULES_MAPPING[AdaptiveMaxPool3d] = fc.pool_flops_counter_hook
-
-except ModuleNotFoundError:  # pragma: no cover
-    pass
-except Exception as e:  # pragma: no cover
-    logger.warning(f"Failed to add flops_counter_hook: {e}")
