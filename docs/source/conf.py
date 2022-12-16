4666c7a624387fa80ec810cf08f18cab02a7f16e
LukasHedegaard <lh@eng.au.dk>
2022-12-16 11:43:42 +0100

    Fix linting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -777,7 +777,7 @@ def patched_make_field(self, types, domain, items, **kw):
                 typename = fieldtype[0].astext()
                 builtin_types = ["int", "long", "float", "bool", "type"]
                 for builtin_type in builtin_types:
-                    pattern = fr"(?<![\w.]){builtin_type}(?![\w.])"
+                    pattern = fr"(?<![\w.]){builtin_type}(?![\w.])"  # noqa: BLK100
                     repl = f"python:{builtin_type}"
                     typename = re.sub(pattern, repl, typename)
                 par.extend(
